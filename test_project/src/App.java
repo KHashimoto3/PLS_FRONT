0a6f625d81a7f139e1171b023a0c1c5bb9d3d068
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-04-26 08:45:26 +0900

    入出力の修正

--- test_project/src/App.java
+++ test_project/src/App.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("aとbの入力をしてください！"); // 出力
-        int a = new java.util.Scanner(System.in).nextInt(); // 値の入力
-        int b = new java.util.Scanner(System.in).nextInt(); // 値の入力
+        Scanner s = new Scanner(System.in); // 入力用に定義
 
-        System.out.println(a * b);
+        System.out.println("文字列を入力してください！"); // 出力
+        String str = s.nextLine(); // 入力
+
+        System.out.println(str);
+
+        // 関数名とその中身に分割
+        int func_name_s; // 関数名の始まり
+        int func_name_e; // 関数名の終わり
+
     }
 }
