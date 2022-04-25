8d586ffa893d5d30107bd546eec5f5eef13e66bc
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-04-25 22:24:52 +0900

    文字の入力と出力をできるようにした。

--- test_project/src/App.java
+++ test_project/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("aとbの入力をしてください！"); // 出力
+        int a = new java.util.Scanner(System.in).nextInt(); // 値の入力
+        int b = new java.util.Scanner(System.in).nextInt(); // 値の入力
+
+        System.out.println(a * b);
     }
 }
