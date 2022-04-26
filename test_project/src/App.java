f3539f431665270181a40067b407df37aed7ead1
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-04-26 14:03:26 +0900

    変数宣言を判定できるようにした。

--- test_project/src/App.java
+++ test_project/src/App.java
@@ -5,10 +5,23 @@ public class App {
     // 変数名を返す関数
     public static String check_variable(String str, int s) {
         String variable_str = "text";
+
+        int e = -1; // 後半の終わり
+
+        // 後半の位置を把握
+        for (int str_idx = s; str_idx < str.length(); str_idx++) {
+            if (str.charAt(str_idx + 1) == ';') { // 中身の最後の位置だったら
+                e = str_idx;
+                break;
+            }
+        }
+
+        variable_str = str.substring(s, e + 1);
+
         return variable_str;
     }
 
-    // 計算式を返す
+    // 代入か計算を表す
     public static String check_culc(String str, int s) {
         String culc_str = "text";
         return culc_str;
@@ -33,12 +46,26 @@ public class App {
         return func_name_str;
     }
 
+    // 変数宣言なのかを確認する関数
+    public static int check_is_declaration(String str) {
+        String[] type = { "int", "double", "float", "char" };
+        int result = 0;
+        // 登録されているどれかの型だったら
+        for (int type_idx = 0; type_idx < type.length; type_idx++) {
+            if (str.equals(type[type_idx])) { // いずれかのタイプだったら
+                result = 1;
+                break;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) throws Exception {
 
         Scanner s = new Scanner(System.in); // 入力用に定義
 
         // 判定結果の配列
-        String[] judge = { "変数", "計算式または代入", "関数" };
+        String[] judge = { "変数宣言", "計算式または代入", "関数" };
         int judge_res = -1;
 
         System.out.println("空白やタブ入りの文字列を入力してください！"); // 出力
@@ -67,6 +94,20 @@ public class App {
                 latter_str = check_function_name(str, first_e + 2);
                 judge_res = 2;
                 break;
+            }
+            // 途中の空白を見つけたら
+            if ((str.charAt(str_idx + 1) == ' ') && (first_s != -1)) {
+                // 変数宣言なのかを確認
+                int result;
+                first_e = str_idx;
+                first_str = str.substring(first_s, first_e + 1);
+                result = check_is_declaration(first_str);
+                if (result == 1) { // 変数宣言だったら
+                    latter_str = check_variable(str, first_e + 2);
+                    judge_res = 0;
+                    break;
+                }
+                // そうで無い場合は処理を継続
             }
         }
 
