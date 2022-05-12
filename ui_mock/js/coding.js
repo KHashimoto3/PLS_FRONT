ba223ef0871bfdee22edc557be27c5c00fffae6f
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-05-12 14:28:59 +0900

    ヒントを吹き出しで表示するようにした。

--- ui_mock/js/coding.js
+++ ui_mock/js/coding.js
@@ -5,6 +5,18 @@ const App = {
       box1: "",
       //タイマー
       timer1: null,
+
+      //吹き出しのstyle
+      timer1_hint_style: {
+        position: "absolute",
+        "background-color": "#808080",
+        color: "white",
+        "font-size": "0.8em",
+        "border-radius": "0.5em",
+        padding: "10px",
+        margin: "-0.8em 0.5em 0 1em",
+        display: "none",
+      },
     };
   },
   methods: {
@@ -15,11 +27,12 @@ const App = {
       this.timer1 = setTimeout(this.show_hint1, 10000);
     },
     show_hint1: function () {
-      console.log("ここには変数の宣言が入ります。");
+      this.timer1_hint_style.display = "inline-block";
     },
     cancel_hint1: function () {
       clearTimeout(this.timer1);
       console.log("ヒントの表示が取り消されました。");
+      this.timer1_hint_style.display = "none";
     },
   },
 };
