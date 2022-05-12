8f8f032649a9618c504bf97bf8580f93fdcf4d8a
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-05-12 13:57:00 +0900

    ボックスをフォーカスしてから10病後にヒントを出すようにした。

--- ui_mock/js/coding.js
+++ ui_mock/js/coding.js
@@ -2,7 +2,25 @@ const App = {
   data() {
     return {
       message: "読み込み完了！",
+      box1: "",
+      //タイマー
+      timer1: null,
     };
+  },
+  methods: {
+    //box1がフォーカスされたら
+    box1_focus: function () {
+      console.log("box1がフォーカスされました。");
+      //タイマーを設定
+      this.timer1 = setTimeout(this.show_hint1, 10000);
+    },
+    show_hint1: function () {
+      console.log("ここには変数の宣言が入ります。");
+    },
+    cancel_hint1: function () {
+      clearTimeout(this.timer1);
+      console.log("ヒントの表示が取り消されました。");
+    },
   },
 };
 
