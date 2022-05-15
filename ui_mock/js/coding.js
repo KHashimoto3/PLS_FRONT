838ad0082bf71a8aa0aae81cef5121573a6c0db3
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-05-15 16:28:44 +0900

    時間ごとに、ヒントが切り替わるようにした。

--- ui_mock/js/coding.js
+++ ui_mock/js/coding.js
@@ -9,11 +9,13 @@ const App = {
       //ヒント
       hint1_txt: "",
       timer1: null,
+      timer2: null,
+      timer3: null,
 
       //吹き出しのstyle
       timer1_hint_style: {
         position: "absolute",
-        "background-color": "#808080",
+        background: "#6cb913d3",
         color: "white",
         "font-size": "0.8em",
         "border-radius": "0.5em",
@@ -27,15 +29,29 @@ const App = {
     //box1がフォーカスされたら
     box1_focus: function () {
       console.log("box1がフォーカスされました。");
-      //タイマーを設定
+      //タイマー1を設定
       if (this.hint1_showed == 0) {
         this.timer1 = setTimeout(this.show_hint1, 10000);
       }
     },
     show_hint1: function () {
-      this.hint1_txt = "変数の型 が入ります。";
+      this.hint1_txt = "この行は　変数を宣言します。";
       this.timer1_hint_style.display = "inline-block";
       this.hint1_showed = 1;
+      //タイマー2を設定
+      this.timer2 = setTimeout(this.show_hint2, 10000);
+    },
+    show_hint2: function () {
+      this.hint1_txt = "ここには　変数の型 が入ります。";
+      this.timer1_hint_style.display = "inline-block";
+      this.timer1_hint_style.background = "#e67b34";
+      //タイマー3を設定
+      this.timer3 = setTimeout(this.show_hint3, 10000);
+    },
+    show_hint3: function () {
+      this.hint1_txt = "変数宣言のサンプルはこちら。";
+      this.timer1_hint_style.display = "inline-block";
+      this.timer1_hint_style.background = "#b913a3d3";
     },
     cancel_hint1: function () {
       clearTimeout(this.timer1);
