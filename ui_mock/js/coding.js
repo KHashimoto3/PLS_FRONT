8ac972b618471fe491855d36727e57b3d6723943
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-05-18 11:45:26 +0900

    各フィールドごとに処理を分割した。

--- ui_mock/js/coding.js
+++ ui_mock/js/coding.js
@@ -4,15 +4,63 @@ const App = {
       box1: "",
       //ヒント使用フラグ
       box1_showed: 0,
+      box2_showed: 0,
+      box3_showed: 0,
+      box4_showed: 0,
 
       //ヒント
       box1_hint_txt: "",
-      timer1: null,
-      timer2: null,
-      timer3: null,
+      box1_t1: null,
+      box1_t2: null,
+      box1_t3: null,
+
+      box2_hint_txt: "",
+      box2_t1: null,
+      box2_t2: null,
+      box2_t3: null,
+
+      box3_hint_txt: "",
+      box3_t1: null,
+      box3_t2: null,
+      box3_t3: null,
+
+      box4_hint_txt: "",
+      box4_t1: null,
+      box4_t2: null,
+      box4_t3: null,
 
       //吹き出しのstyle
       box1_style: {
+        position: "absolute",
+        background: "#6cb913d3",
+        color: "white",
+        "font-size": "0.8em",
+        "border-radius": "0.5em",
+        padding: "10px",
+        margin: "-0.8em 0.5em 0 1em",
+        display: "none",
+      },
+      box2_style: {
+        position: "absolute",
+        background: "#6cb913d3",
+        color: "white",
+        "font-size": "0.8em",
+        "border-radius": "0.5em",
+        padding: "10px",
+        margin: "-0.8em 0.5em 0 1em",
+        display: "none",
+      },
+      box3_style: {
+        position: "absolute",
+        background: "#6cb913d3",
+        color: "white",
+        "font-size": "0.8em",
+        "border-radius": "0.5em",
+        padding: "10px",
+        margin: "-0.8em 0.5em 0 1em",
+        display: "none",
+      },
+      box4_style: {
         position: "absolute",
         background: "#6cb913d3",
         color: "white",
@@ -30,7 +78,7 @@ const App = {
       console.log("box1がフォーカスされました。");
       //タイマー1を設定
       if (this.box1_showed == 0) {
-        this.timer1 = setTimeout(this.show_box1_hint1, 10000);
+        this.box1_t1 = setTimeout(this.show_box1_hint1, 10000);
       }
     },
     show_box1_hint1: function () {
@@ -38,14 +86,14 @@ const App = {
       this.box1_style.display = "inline-block";
       this.box1_showed = 1;
       //タイマー2を設定
-      this.timer2 = setTimeout(this.show_box1_hint2, 10000);
+      this.box1_t2 = setTimeout(this.show_box1_hint2, 10000);
     },
     show_box1_hint2: function () {
       this.box1_hint_txt = "ここには　変数の型 が入ります。";
       this.box1_style.display = "inline-block";
       this.box1_style.background = "#e67b34";
       //タイマー3を設定
-      this.timer3 = setTimeout(this.show_box1_hint3, 10000);
+      this.box1_t3 = setTimeout(this.show_box1_hint3, 10000);
     },
     show_box1_hint3: function () {
       this.box1_hint_txt = "変数宣言のサンプルはこちら。";
@@ -53,7 +101,103 @@ const App = {
       this.box1_style.background = "#b913a3d3";
     },
     cancel_box1: function () {
-      clearTimeout(this.timer1);
+      clearTimeout(this.box1_t1);
+      console.log("ヒントの表示が取り消されました。");
+    },
+
+    //box2がフォーカスされる
+    box2_focus: function () {
+      console.log("box1がフォーカスされました。");
+      //タイマー1を設定
+      if (this.box2_showed == 0) {
+        this.box2_t1 = setTimeout(this.show_box2_hint1, 10000);
+      }
+    },
+    show_box2_hint1: function () {
+      this.box2_hint_txt = "この行は　変数を宣言します。";
+      this.box2_style.display = "inline-block";
+      this.box2_showed = 1;
+      //タイマー2を設定
+      this.box2_t2 = setTimeout(this.show_box2_hint2, 10000);
+    },
+    show_box2_hint2: function () {
+      this.box2_hint_txt = "ここには　変数の型 が入ります。";
+      this.box2_style.display = "inline-block";
+      this.box2_style.background = "#e67b34";
+      //タイマー3を設定
+      this.box2_t3 = setTimeout(this.show_box2_hint3, 10000);
+    },
+    show_box2_hint3: function () {
+      this.box2_hint_txt = "変数宣言のサンプルはこちら。";
+      this.box2_style.display = "inline-block";
+      this.box2_style.background = "#b913a3d3";
+    },
+    cancel_box2: function () {
+      clearTimeout(this.box2_t1);
+      console.log("ヒントの表示が取り消されました。");
+    },
+
+    //box3がフォーカスされる
+    box3_focus: function () {
+      console.log("box1がフォーカスされました。");
+      //タイマー1を設定
+      if (this.box3_showed == 0) {
+        this.box3_t1 = setTimeout(this.show_box3_hint1, 10000);
+      }
+    },
+    show_box3_hint1: function () {
+      this.box3_hint_txt = "この行は　変数を宣言します。";
+      this.box3_style.display = "inline-block";
+      this.box3_showed = 1;
+      //タイマー2を設定
+      this.box3_t2 = setTimeout(this.show_box3_hint2, 10000);
+    },
+    show_box3_hint2: function () {
+      this.box3_hint_txt = "ここには　変数の型 が入ります。";
+      this.box3_style.display = "inline-block";
+      this.box3_style.background = "#e67b34";
+      //タイマー3を設定
+      this.box3_t3 = setTimeout(this.show_box3_hint3, 10000);
+    },
+    show_box3_hint3: function () {
+      this.box3_hint_txt = "変数宣言のサンプルはこちら。";
+      this.box3_style.display = "inline-block";
+      this.box3_style.background = "#b913a3d3";
+    },
+    cancel_box3: function () {
+      clearTimeout(this.box3_t1);
+      console.log("ヒントの表示が取り消されました。");
+    },
+
+    //box4がフォーカスされる
+    box4_focus: function () {
+      console.log("box1がフォーカスされました。");
+      //タイマー1を設定
+      if (this.box4_showed == 0) {
+        this.box4_t1 = setTimeout(this.show_box4_hint1, 10000);
+      }
+    },
+    show_box4_hint1: function () {
+      this.box4_hint_txt = "この行は　変数を宣言します。";
+      this.box4_style.display = "inline-block";
+      this.box4_showed = 1;
+      //タイマー2を設定
+      this.box4_t2 = setTimeout(this.show_box4_hint2, 10000);
+    },
+    show_box4_hint2: function () {
+      this.box4_hint_txt = "ここには　変数の型 が入ります。";
+      this.box4_style.display = "inline-block";
+      this.box4_style.background = "#e67b34";
+      //タイマー3を設定
+      this.box4_t3 = setTimeout(this.show_box4_hint3, 10000);
+    },
+    show_box4_hint3: function () {
+      this.box4_hint_txt = "変数宣言のサンプルはこちら。";
+      this.box4_style.display = "inline-block";
+      this.box4_style.background = "#b913a3d3";
+    },
+    cancel_box4: function () {
+      clearTimeout(this.box4_t1);
       console.log("ヒントの表示が取り消されました。");
     },
   },
