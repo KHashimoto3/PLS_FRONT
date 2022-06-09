188f751e114d3d04f1394fd708298b1c0cfbb9cc
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-06-09 13:28:25 +0900

    vue.config.jsを、複数ページ書けるように変更

--- teikei_proto/vue.config.js
+++ teikei_proto/vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+//const { defineConfig } = require('@vue/cli-service')
+
+module.exports = {
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+      title: "Index Page2",
+    },
+  },
+};
+/*module.exports = defineConfig({
   transpileDependencies: true
-})
+})*/
