ccb85f8ca4009e6d9972f4bb3567ae2d71b81b75
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-06-13 11:43:20 +0900

    動的にフォームを切り替えるようにするための準備

--- teikei_proto/src/CodingForm.vue
+++ teikei_proto/src/CodingForm.vue
@@ -1,17 +1,21 @@
 <template>
   <section>
     <h1>繰り返し処理</h1>
-    <FormRoop />
+    <FormRoop ref="formRoop" />
+    <FormFunc ref="FormFunc" />
   </section>
 </template>
 
 <script>
 import FormRoop from "./components/FormRoop.vue";
+import FormFunc from "./components/FormFunction.vue";
 
 export default {
   name: "CodingForm",
   components: {
     FormRoop,
+    FormFunc,
   },
+  mounted() {},
 };
 </script>
