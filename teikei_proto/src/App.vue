41c63a6b9e33cca788cdfcc1337e311dae788402
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-06-13 14:22:23 +0900

    学籍番号をパラメータとして渡すようにした

--- teikei_proto/src/App.vue
+++ teikei_proto/src/App.vue
@@ -15,6 +15,7 @@
     <p>
       実験手順書に従い、以下のリンクをクリックしてコーディングを開始してください。
     </p>
+    学籍番号：<input type="text" v-model="student_id" required />
     <a @click="goto_form(0)">A問題</a><br />
     <a @click="goto_form(1)">B問題</a>
   </section>
@@ -23,11 +24,17 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      student_id: "", //学籍番号
+    };
+  },
   methods: {
     //form_idをCodingFormに渡す
     goto_form: function (form_id) {
       //表示するform_idを取得
-      const url = "CodingForm.html?id=" + form_id;
+      const url =
+        "CodingForm.html?id=" + form_id + "&student=" + this.student_id;
       //idのパラメータを含むurlに遷移
       location.href = url;
     },
