02dfff93fed7fd483bfa99c5158c60c6115f6afd
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-07-08 16:32:04 +0900

    他の構成要素のチェックもできるように準備

--- pattern_test/js/main.js
+++ pattern_test/js/main.js
@@ -6,16 +6,18 @@ const App = {
     };
   },
   methods: {
-    check_pattern: function () {
+    check_pattern: function (id) {
       //正規表現をチェック
       //const result = this.txt.match(/^\d{3}-?\d{4}$/);
-      const result = this.txt.match(
-        /(^\sint\s\w+;)|(^\sint\s\w+\s=\s[0-9]+;)|(^\sdouble\s\w+;)|(^\sdouble\s\w+\s=\s([0-9]|\.)+;)|(^\schar\s\w+;)|(^\schar\s\w+\s=\s'\w';)/
-      );
-      if (result == null) {
-        this.result = "不正";
-      } else {
-        this.result = "適合";
+      if (id == 4) {
+        const result = this.txt.match(
+          /(^\sint\s\w+;)|(^\sint\s\w+\s=\s[0-9]+;)|(^\sdouble\s\w+;)|(^\sdouble\s\w+\s=\s([0-9]|\.)+;)|(^\schar\s\w+;)|(^\schar\s\w+\s=\s'\w';)/
+        );
+        if (result == null) {
+          this.result = "不正";
+        } else {
+          this.result = "適合";
+        }
       }
     },
   },
