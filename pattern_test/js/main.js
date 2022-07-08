fc273ec7cbfdba4ca7c6e073467035ce51042e54
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-07-08 16:17:03 +0900

    一部の変数宣言のスタイルチェックに対応

--- pattern_test/js/main.js
+++ pattern_test/js/main.js
@@ -9,7 +9,9 @@ const App = {
     check_pattern: function () {
       //正規表現をチェック
       //const result = this.txt.match(/^\d{3}-?\d{4}$/);
-      const result = this.txt.match(/^\d{3}-?\d{4}$/);
+      const result = this.txt.match(
+        /(^\sint\s\w+;)|(^\sint\s\w+\s=\s[0-9]+;)|(^\sdouble\s\w+;)|(^\sdouble\s\w+\s=\s([0-9]|\.)+;)|(^\schar\s\w+;)/
+      );
       if (result == null) {
         this.result = "不正";
       } else {
