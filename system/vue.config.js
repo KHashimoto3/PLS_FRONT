e738a2b16365823f1d04cef034db6e211a680a0e
橋本浩規 <khashimoto@KHnoMacBook-Pro.local>
2022-07-13 16:19:59 +0900

    開発のための下準備

--- system/vue.config.js
+++ system/vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+//const { defineConfig } = require('@vue/cli-service')
+
+module.exports = {
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+      title: "ホーム",
+    },
+  },
+};
+/*module.exports = defineConfig({
   transpileDependencies: true
-})
+})*/
