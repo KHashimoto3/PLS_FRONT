b1cf2db1d137b5e968e4ebdd2c06f6e2aa43fc6b
KHashimoto3 <hashi.koki385@gmail.com>
2022-08-23 11:14:12 +0900

    bootstrapの使用を中止した。

--- system/src/components/CodeRun.vue
+++ system/src/components/CodeRun.vue
@@ -36,6 +36,20 @@
         <textarea cols="55" rows="8" v-model="errTxt" disabled></textarea><br />
       </p>
     </div>
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
   </div>
 </template>
 
