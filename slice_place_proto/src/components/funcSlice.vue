a39cc1eda460c4c3ce28c4999a7805df560d3924
KHashimoto3 <hashi.koki385@gmail.com>
2022-10-03 13:32:05 +0900

    funcSliceコンポーネントを用意して読み込んだ

---
+++ slice_place_proto/src/components/funcSlice.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="funcSliceArea">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "funcSlice",
+  data() {
+    return {
+      text: "hello!!!!",
+    };
+  },
+};
+</script>
