4a5644c1513c612367a525abc595330c6650657a
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2022-12-25 13:47:34 +0900

    すべての文法フォームでコードをコピーできるようにした。

--- system/src/components/if_form.vue
+++ system/src/components/if_form.vue
@@ -260,7 +260,22 @@ export default {
       if (this.elseIsUsed) {
         connectedCode = connectedCode + " else {\n" + this.elseBodyTxt + "\n}";
       }
-      return connectedCode;
+      this.copyToClipboard(connectedCode);
+    },
+    copyToClipboard: function (text) {
+      // テキストコピー用の一時要素を作成
+      const pre = document.createElement("pre");
+      // テキストを選択可能にしてテキストセット
+      pre.style.webkitUserSelect = "auto";
+      pre.style.userSelect = "auto";
+      pre.textContent = text;
+      // 要素を追加、選択してクリップボードにコピー
+      document.body.appendChild(pre);
+      document.getSelection().selectAllChildren(pre);
+      const result = document.execCommand("copy");
+      // 要素を削除
+      document.body.removeChild(pre);
+      return result;
     },
   },
 };
