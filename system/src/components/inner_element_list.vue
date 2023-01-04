2a694d4d6e059cf916b4e84b39cd9ce4c7422814
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-01-04 12:50:00 +0900

    構成要素を詳細に表示するコンポーネントを作成した。

---
+++ system/src/components/inner_element_list.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="inner_element">
+    <h1>STEP{{ stepNo }}</h1>
+    <p>{{ elemetName }}のSTEPは以下の要素で構成されます。</p>
+    <div class="elementTableArea">
+      <table>
+        <tr>
+          <th>番号</th>
+          <th>要素名</th>
+          <th>文法</th>
+        </tr>
+        <tr v-for="i of count" :key="i">
+          <td>{{ i + 1 }}</td>
+          <td>あああ</td>
+          <td>あああ</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "inner_element_list",
+  data() {
+    return {
+      stepNo: 1,
+      count: 3,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
