a78ced2344c2a8327c2933aef9e81178155b1421
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-01-07 17:22:21 +0900

    アカウント管理のページを作成した。

--- system/src/LoginPage.vue
+++ system/src/LoginPage.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <header-comp />
+    <div class="accountInfoArea">
+      <div class="iconArea">
+        <img class="userIcon" src="@/assets/user.png" alt="user_icon" />
+      </div>
+      <div class="nameArea">
+        <h1>ゲストユーザ</h1>
+      </div>
+      <div class="buttonArea">
+        <button class="css-button-rounded--login">ログイン</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,7 +31,59 @@ export default {
 </script>
 
 <style scoped>
-a {
+div.accountInfoArea {
+  font-family: sans-serif;
+  width: 80%;
+  height: 400px;
+  margin: 80px auto 10px auto;
+}
+div.iconArea {
+  text-align: center;
+  width: 20%;
+  height: 100%;
+  float: left;
+}
+div.nameArea {
+  width: 50%;
+  height: 100%;
+  padding: 10px;
+  float: left;
+}
+div.buttonArea {
+  width: 30%;
+  height: 100%;
+  margin-left: 70%;
+  padding-top: 30px;
+  text-align: center;
+}
+
+button.css-button-rounded--login {
+  font-size: 18pt;
+  min-width: 130px;
+  height: 40px;
+  color: #fff;
+  padding: 5px 10px;
+  font-weight: bold;
   cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+  outline: none;
+  border-radius: 5px;
+  border: 2px solid #57cc99;
+  background: #57cc99;
+}
+button.css-button-rounded--login:hover {
+  background: #fff;
+  color: #57cc99;
+}
+button.css-button-rounded--login:disabled {
+  color: #fff;
+  border: 2px solid #a7cfbe;
+  background: #a7cfbe;
+}
+img.userIcon {
+  width: 100px;
+  height: auto;
 }
 </style>
