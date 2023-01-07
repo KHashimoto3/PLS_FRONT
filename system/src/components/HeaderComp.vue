059d50efc2acd6589f3daa349994a114531af2ac
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-01-07 12:00:21 +0900

    ヘッダの構成を変更し、スタイルを整えた。

--- system/src/components/HeaderComp.vue
+++ system/src/components/HeaderComp.vue
@@ -2,12 +2,17 @@
   <div class="header">
     <div class="headerLeft">
       <div class="headerLeftInner">
-        {{ title }}
+        <p>{{ backButtonTxt }}</p>
+      </div>
+    </div>
+    <div class="headerMiddle">
+      <div class="headerMiddleInner">
+        <p>{{ title }}</p>
       </div>
     </div>
     <div class="headerRight">
       <div class="headerRightInner" @click="gotoLoginPage()">
-        {{ loginUserName }}
+        <p>{{ loginUserName }}</p>
       </div>
     </div>
   </div>
@@ -18,6 +23,7 @@ export default {
   name: "HeaderComp",
   data() {
     return {
+      backButtonTxt: "ホーム",
       title: "ヘッダタイトル",
       loginUserName: "ログイン",
     };
@@ -39,18 +45,32 @@ div.header {
   box-shadow: 0px 10px 10px -5px rgba(0, 0, 0, 0.5);
   font-size: 20pt;
 }
+div.header p {
+  font-family: sans-serif;
+  margin: 0;
+}
 div.headerLeft {
-  width: 80%;
+  width: 20%;
   height: 100%;
-  float: left;
   background: #ffffff;
   display: flex;
   text-align: left;
+  float: left;
+}
+div.headerMiddle {
+  width: 60%;
+  height: 100%;
+  margin-left: 0;
+  background: #ffffff;
+  display: flex;
+  float: left;
+  text-align: center;
 }
 div.headerRight {
   width: 20%;
   height: 100%;
-  margin-left: 20%;
+  margin-left: 60%;
+  color: #ffffff;
   background: #57cc99;
   display: flex;
   text-align: center;
@@ -59,6 +79,11 @@ div.headerRight:hover {
   cursor: pointer;
 }
 div.headerLeftInner {
+  width: 90%;
+  height: 70%;
+  margin: auto;
+}
+div.headerMiddleInner {
   width: 90%;
   height: 70%;
   margin: auto;
