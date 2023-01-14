a78c41daf5b633789875b9dc7146777a255dc021
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-01-14 12:22:27 +0900

    ヘッダのデザインを変更した。

--- system/src/components/HeaderComp.vue
+++ system/src/components/HeaderComp.vue
@@ -58,7 +58,8 @@ div.header {
   width: 100%;
   height: 60px;
   margin: 0;
-  box-shadow: 0px 10px 10px -5px rgba(0, 0, 0, 0.5);
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px,
+    rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
   font-size: 20pt;
 }
 div.header p {
