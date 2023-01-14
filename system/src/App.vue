adbfbe2c157ca15cfccd699e40a99c56b4d81e8a
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-01-14 15:11:19 +0900

    フォーム一覧のカードのデザインを調整した。

--- system/src/App.vue
+++ system/src/App.vue
@@ -11,9 +11,15 @@
           <div class="formCard" v-for="i of formListDataObj" :key="i">
             <div class="formCardImg"></div>
             <div class="formCardTitle">
-              <p>
-                <a @click="selectForm(i.id)">{{ i.name }}</a>
-              </p>
+              <h1>{{ i.name }}</h1>
+            </div>
+            <div class="formCardButton">
+              <button
+                class="css-button-rounded--green"
+                @click="selectForm(i.id)"
+              >
+                始める
+              </button>
             </div>
           </div>
         </div>
@@ -92,7 +98,7 @@ div.contentsArea {
 }
 div.mainTextArea {
   width: 100%;
-  height: 400px;
+  height: 200px;
   background-image: linear-gradient(
     90deg,
     rgba(89, 173, 241, 1),
@@ -102,7 +108,7 @@ div.mainTextArea {
 div.mainTextArea h1 {
   text-align: center;
   color: #ffffff;
-  padding: 150px;
+  padding: 60px;
   font-size: 40pt;
 }
 
@@ -127,14 +133,41 @@ div.formCard {
 div.formCardImg {
   width: 100%;
   height: 40%;
-  background-image: linear-gradient(
-    90deg,
-    rgba(57, 210, 248, 1),
-    rgba(48, 97, 96, 1)
-  );
+  background-image: linear-gradient(135deg, #79f1a4 10%, #0e5cad 100%);
 }
 div.formCardTitle {
   width: 100%;
-  height: 60%;
+  height: 40%;
+}
+div.formCardButton {
+  width: 100%;
+  height: 20%;
+  text-align: center;
+}
+
+button.css-button-rounded--green {
+  font-size: 18pt;
+  min-width: 130px;
+  height: 40px;
+  color: #fff;
+  padding: 5px 10px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+  outline: none;
+  border-radius: 5px;
+  border: 2px solid #57cc99;
+  background: #57cc99;
+}
+button.css-button-rounded--green:hover {
+  background: #fff;
+  color: #57cc99;
+}
+button.css-button-rounded--green:disabled {
+  color: #fff;
+  border: 2px solid #a7cfbe;
+  background: #a7cfbe;
 }
 </style>
